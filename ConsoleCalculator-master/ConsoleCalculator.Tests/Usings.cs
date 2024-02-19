946526bbdbe36fb93240e5cb992ec09f2b808e4b
Amanda Morgan <69015861+thepandaknows@users.noreply.github.com>
2024-02-18 18:42:40 -0800

    Delete ConsoleCalculator-master directory

--- ConsoleCalculator-master/ConsoleCalculator.Tests/Usings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using ConsoleCalculator;
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
