e9d4032553d8a6472ad82b4ee2be28018fa9ac06
Amanda Morgan <69015861+thepandaknows@users.noreply.github.com>
2024-02-18 21:09:07 -0600

    initial commit
    Had to manually upload

---
+++ LearnMyCalculatorApp/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace LearnMyCalculatorApp
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
